ae67c5f23aba3532657db9bb02291cad500d4ec7
MohamedMohsen94 <m.mohsen28894@gmail.com>
2022-10-25 14:28:22 +0200

    Last commit

--- spies/app.test.js
+++ spies/app.test.js
@@ -1,6 +1,6 @@
 let expect = require("expect");
+// let app = require(./app.js);
 let rewire = require("rewire");
-
 let app = rewire("./app.js");
 
 describe("app", () => {
@@ -10,8 +10,8 @@ describe("app", () => {
   app.__set__("db", db);
 
   it("it should call the spy correctly", () => {
-    let spy = expect.createSpy();
-    spy("Mohamed", 25);
+    let spy = expect.createSpy(); // created a spy and put it in a
+    spy("Mohamed", 25); // for calling the spy
     expect(spy).toHaveBeenCalledWith("Mohamed", 25);
   });
   it("it should call saveUser with user object", () => {
